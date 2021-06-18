5dd88942f69c17650a8b9b9a828bb4c54b8b12b0
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-06-18 07:21:00 +0700

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import(/* webpackChunkName: "route-contact" */ '../views/Contact.vue')
+  },
+  
+]
+
+const router = new VueRouter({
+  mode: process.env.VUE_APP_ROUTE_MODE,
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
